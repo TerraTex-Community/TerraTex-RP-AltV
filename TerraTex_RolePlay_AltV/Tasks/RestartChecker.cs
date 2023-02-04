fcfd50c441761a4125cc674dbcc5da803f44fe74
JessyCat92 <jessica.weber.92@outlook.de>
2023-02-04 19:29:56 +0100

    add taks

---
+++ TerraTex_RolePlay_AltV/Tasks/RestartChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Timers;
+using AltV.Net;
+
+namespace TerraTex_RolePlay_AltV_Server.Tasks;
+
+public class RestartChecker: IDisposable
+{
+    private System.Timers.Timer _timer;
+
+    public RestartChecker()
+    {
+        _timer = new System.Timers.Timer();
+        _timer.Interval = 5000;
+        _timer.Elapsed += TimerOnElapsed;
+        _timer.Start();
+    }
+
+    private void TimerOnElapsed(object? sender, ElapsedEventArgs e)
+    {
+        if (File.Exists("stop.command"))
+        {
+            Console.WriteLine("Found Stop Command ... Start Server Shutdown");
+            Alt.Core.StopServer();
+        }
+    }
+
+    public void Dispose()
+    {
+        _timer.Stop();
+    }
+}
