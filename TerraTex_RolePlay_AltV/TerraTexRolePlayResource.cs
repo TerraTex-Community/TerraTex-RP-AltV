0b1e096d874d71fbf37b72af0e4333e32530df25
JessyCat92 <jessica.weber.92@outlook.de>
2023-02-04 17:38:57 +0100

    push changes

--- TerraTex_RolePlay_AltV/TerraTexRolePlayResource.cs
+++ TerraTex_RolePlay_AltV/TerraTexRolePlayResource.cs
@@ -1,4 +1,5 @@
-﻿using AltV.Net;
+﻿using System.Diagnostics;
+using AltV.Net;
 using AltV.Net.Async;
 using AltV.Net.Elements.Entities;
 using AltV.Net.Shared.Elements.Data;
@@ -7,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using TerraTex_RolePlay_AltV_Server.CustomFactories;
 using TerraTex_RolePlay_AltV_Server.Database;
+using TerraTex_RolePlay_AltV_Server.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace TerraTex_RolePlay_AltV_Server
@@ -15,6 +17,17 @@ namespace TerraTex_RolePlay_AltV_Server
     {
         public override void OnStart()
         {
+            Process currentProcess = Process.GetCurrentProcess();
+            if (File.Exists("pid.txt"))
+            {
+                File.Delete("pid.txt");
+            }
+            File.WriteAllText("pid.txt", currentProcess.Id.ToString());
+
+            // Start Restart Checker Task
+            new RestartChecker();
+
+
             Alt.Core.RegisterCustomCommands();
             Console.WriteLine("TerraTex Server started");
 
@@ -26,6 +39,9 @@ namespace TerraTex_RolePlay_AltV_Server
         public override void OnStop()
         {
             Console.WriteLine("TerraTex Server Stopped");
+
+            File.Delete("pid.txt");
+            File.Delete("stop.command");
         }
 
         public override IEntityFactory<IPlayer> GetPlayerFactory()
--- todo.md
+++ todo.md
@@ -2,7 +2,6 @@
 
 1) Deployment Local?
 2) Create Server and Database on Root
-3) Add Git
 4) Deployment on Server?
 
 5) Start Happy Coding
